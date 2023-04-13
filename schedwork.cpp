6bac871dd9a6fdec820a85d5550573e2f0c2a28b
Adam Lewczuk <lewczuk@usc.edu>
2023-04-13 05:32:33 +0000

    hw5 final turn in

--- schedwork.cpp
+++ schedwork.cpp
@@ -6,10 +6,9 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-// add or remove necessary headers as you please
-
 #endif
 
+// add or remove necessary headers as you please
 #include "schedwork.h"
 
 using namespace std;
@@ -21,7 +20,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, int row, int col, std::map<int, int>& map);
+bool check(const size_t maxShifts, const int& workers, std::map<int, int>& map);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -36,10 +36,67 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
+
+    unsigned int numDays = avail.size();
+    std::vector<unsigned int> newvec(dailyNeed, INVALID_ID);
+    int row = 0;
+    int col = 0;
 
 
+    std::map<int, int> wToS;
+    for (unsigned int i = 0; i < avail[0].size(); i++){
+        wToS.insert(std::make_pair(i, 0));
+    }
+    
+    
+    for (unsigned int i = 0; i < numDays; i++){
+        sched.push_back(newvec);
+    }
 
-
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, row, col, wToS);
 }
 
+
+bool scheduleHelper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, int row, int col, std::map<int, int>& map){
+		int days = sched.size();
+		
+		if (row >= days){
+			return true;
+    } else if (col >= (int)dailyNeed){
+        col = 0;
+        row++;
+        return scheduleHelper(avail, dailyNeed, maxShifts, sched, row, col, map);
+    }
+
+		if (sched[row][col] != INVALID_ID){
+			col++;
+			return scheduleHelper(avail, dailyNeed, maxShifts, sched, row, col, map);
+		} else {
+			unsigned int workers = avail[0].size();
+			int temp = 0;
+			for (unsigned int i = 0; i < workers; i++){
+				sched[row][col] = i;
+				if ((avail[row][i] != 0) && (check(maxShifts, i, map))){
+					std::map<int, int>::iterator it = map.find(i);
+					temp = it->second + 1;
+					it->second = temp;
+
+					col++;
+					if (scheduleHelper(avail, dailyNeed, maxShifts, sched, row, col, map)) {
+						return true;
+					}
+				}
+				sched[row][col] = INVALID_ID;
+			}
+		}
+
+    return false;
+}
+
+
+bool check(const size_t maxShifts, const int& workers, std::map<int, int>& map){
+    std::map<int, int>::iterator it = map.find(workers);
+		unsigned int temp = it->second + 1;
+
+    return temp <= maxShifts;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -4,6 +4,7 @@
 // For std::remove
 #include <algorithm> 
 #include <map>
+#include <vector>
 #include <set>
 #endif
 
@@ -12,17 +13,107 @@
 using namespace std;
 
 
-// Add prototypes of helper functions here
+void flInsert(const int& len, const int& fllen, const std::vector<int>& positions, std::vector<int>& temp, const std::set<std::string>& dict, std::string& in, const std::string& floating, std::set<std::string>& ans);
+void wordleHelper(const int& len, std::string& in, std::set<std::string>& ans, const std::set<std::string>& dict, int& lentemp);
+bool isValid(const std::set<std::string>& dict, const std::string& in);
 
 
-// Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    std::set<std::string> ret = {};
+		if (in == ""){
+			return ret;
+		}
 
+    std::string temp = in;
+    int length = in.length();
+		int flolen = floating.length();
+		std::vector<int> pos = {};
+		std::vector<int> tem = pos;
+		
+		for (int i = 0; i < length; i++){
+			if (in[i] == '-'){
+				pos.push_back(i);
+			}
+		}
+		flInsert(length, flolen, pos, tem, dict, temp, floating, ret);
+
+    return ret;
 }
 
-// Define any helper functions here
+
+void flInsert(const int& len, const int& fllen, const std::vector<int>& positions, std::vector<int>& temp, const std::set<std::string>& dict, std::string& in, const std::string& floating, std::set<std::string>& ans){
+		if (fllen == 0){
+			int lentemp = 0;
+			wordleHelper(len, in, ans, dict, lentemp);
+			return;
+		}
+		
+		if ((int)temp.size() == fllen){
+			for (unsigned int i = 0; i < temp.size(); i++){
+				in[temp[i]] = floating[i];
+			}
+			int lentemp = 0;
+			wordleHelper(len, in, ans, dict, lentemp);
+
+			int val = temp[temp.size()-1];
+			in[val] = '-';
+			temp.pop_back();
+			return;
+		}
+
+		for (unsigned int i = 0; i < positions.size(); i++){
+			std::vector<int>::iterator it = std::find(temp.begin(), temp.end(), positions[i]);
+			if (it == temp.end()){
+				temp.push_back(positions[i]);
+				flInsert(len, fllen, positions, temp, dict, in, floating, ans);
+			}
+		}
+
+		int val = temp[temp.size()-1];
+		in[val] = '-';
+		temp.pop_back();
+		return;
+}
+
+void wordleHelper(const int& len, std::string& in, std::set<std::string>& ans, const std::set<std::string>& dict, int& lentemp){
+		if (lentemp == len){
+			if (isValid(dict, in)){
+				ans.insert(in);
+			}
+			lentemp--;
+			return;
+		}
+
+		if (in[lentemp] != '-'){
+			lentemp++;
+			wordleHelper(len, in, ans, dict, lentemp);
+			lentemp--;
+			return;
+		} else {
+				for (char c = 'a'; c <= 'z'; c++){
+					in[lentemp] = c;
+					lentemp++;
+					wordleHelper(len, in, ans, dict, lentemp);
+				}
+				in[lentemp] = '-';
+		}
+
+		lentemp--;
+		return;
+}
+
+
+bool isValid(const std::set<std::string>& dict, const std::string& in){
+    std::set<std::string>::iterator it;
+    it = dict.find(in);
+
+    if (it != dict.end()){
+        return true;
+    } else {
+        return false;
+    }
+}
